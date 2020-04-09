14a66369af5d466f8ee452dd25ac676211041937
Greyson Christoforo <grey@christoforo.net>
2020-04-09 16:58:27 +0100

    Revert "inputflinger: treat tablet-style inputs as absolute coordinate mouse pointer"
    This reverts commit bf8d48c1742e9df94bf25742248a1a282011f5cc.

--- services/inputflinger/InputReader.h
+++ services/inputflinger/InputReader.h
@@ -678,16 +678,10 @@ public:
 
     inline int32_t getRelativeX() const { return mRelX; }
     inline int32_t getRelativeY() const { return mRelY; }
-    inline int32_t getAbsoluteX() const { return mAbsX; }
-    inline int32_t getAbsoluteY() const { return mAbsY; }
-    inline bool hasMoved() const { return mMoved; }
 
 private:
     int32_t mRelX;
     int32_t mRelY;
-    int32_t mAbsX;
-    int32_t mAbsY;
-    bool mMoved;
 
     void clearRelativeAxes();
 };
@@ -1181,7 +1175,6 @@ private:
         Mode mode;
         bool hasAssociatedDisplay;
         bool orientationAware;
-        bool hasAbsAxis;
     } mParameters;
 
     CursorButtonAccumulator mCursorButtonAccumulator;
@@ -1189,8 +1182,6 @@ private:
     CursorScrollAccumulator mCursorScrollAccumulator;
 
     int32_t mSource;
-    RawAbsoluteAxisInfo mRawAbsXInfo;
-    RawAbsoluteAxisInfo mRawAbsYInfo;
     float mXScale;
     float mYScale;
     float mXPrecision;
@@ -1214,7 +1205,7 @@ private:
 
     void configureParameters();
     void dumpParameters(String8& dump);
-    void rotateAbsolute(float* absX, float* absY);
+
     void sync(nsecs_t when);
 };
 
